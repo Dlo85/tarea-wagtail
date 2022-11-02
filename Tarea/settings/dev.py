7978d3604292d8096182f05196abbc145b58b10d
Daniel Leon <danileonortega0585@gmail.com>
2022-11-01 23:03:54 -0500

    update

---
+++ Tarea/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-45rj=@z7uuhqz#x44_5!n^#8tfsh74gf9aa&)y-kxxk$7-f8e)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
